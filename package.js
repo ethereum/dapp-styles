1f75189d01cfc6e60f5d0cc126b010975f84e914
Fabian Vogelsteller <fabian@frozeman.de>
2015-10-15 16:49:01 +0200

    fixed header buttons

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:dapp-styles',
   summary: 'CSS/LESS framework for dapps',
-  version: '0.2.6',
+  version: '0.2.7',
   git: 'http://github.com/ethereum/dapp-styles'
 });
 
