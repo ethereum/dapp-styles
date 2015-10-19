67e5f02d7c71373e949a900766d477d73452f7ba
Fabian Vogelsteller <fabian@frozeman.de>
2015-10-19 16:49:12 +0200

    bumped version

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:dapp-styles',
   summary: 'CSS/LESS framework for dapps',
-  version: '0.2.8',
+  version: '0.2.9',
   git: 'http://github.com/ethereum/dapp-styles'
 });
 
