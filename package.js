509292ee98a1d46b8e9340a7430515e3dbff064f
Fabian Vogelsteller <fabian@frozeman.de>
2015-10-21 14:08:02 +0200

    fixed dapp-horizontal-menu

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:dapp-styles',
   summary: 'CSS/LESS framework for dapps',
-  version: '0.2.9',
+  version: '0.3.0',
   git: 'http://github.com/ethereum/dapp-styles'
 });
 
