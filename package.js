9e54eddc76056e9b90c3bb5f801a57605f4a65c8
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2016-07-14 16:58:08 -0300

    version 0.5.6 brigther default blue

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ethereum:dapp-styles',
   summary: 'CSS/LESS framework for dapps',
-  version: '0.5.5',
+  version: '0.5.6',
   git: 'http://github.com/ethereum/dapp-styles'
 });
 
